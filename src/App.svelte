b3eaefde46d85f15eeb139cff052107950d8f4be
Dveneziale <dominicveneziale24@gmail.com>
2025-09-25 18:32:14 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<br> <br> <br> <br>
+
+ <select name="team_number" class="select select-bordered w-full max-w-xs" required>
+      <option value = "834"> 834 Spartechs </option>
+    </select>
+    <br>
+
