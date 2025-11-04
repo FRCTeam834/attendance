1a5ccbc47dbbc619daab77d992efe728d48068d7
Dveneziale <dominicveneziale24@gmail.com>
2025-11-04 18:39:34 -0500

    fixed last line

--- backend.py
+++ backend.py
@@ -147,4 +147,4 @@ def get_attendance():
 # ---------------------------
 if __name__ == "__main__":
     logging.info("Starting Flask server on port 5000...")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False)
