ad0d69c5d71d314b1a230a2d8385d1c5dbc9aaf6
Dominic Veneziale <you@example.com>
2025-11-10 23:44:25 -0500

    buttons

--- src/App.svelte
+++ src/App.svelte
@@ -110,188 +110,161 @@
   onMount(initCanvas);
 </script>
 
-<style>
-  .center-wrapper {
-    text-align: center;
-  }
-
-  canvas {
-    border: 8px solid #ffffff;
-    background-color: rgb(65, 105, 225);
-    cursor: crosshair;
-    margin-top: 20px;
-  }
-
-  .form-container {
-    display: inline-block;
-    text-align: center;
-  }
-
-  select, button {
-    margin: 10px;
-  }
-</style>
-
+<!-- ============== YOUR DECORATIVE IMAGES (unchanged) ============== -->
 <a href="https://imgbb.com/">
-  <img 
-    src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png" 
+  <img
+    src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png"
     alt="Flower image"
-    style="width: 125px; height: auto; position: absolute; top: 150px; left: 450px;" />
+    style="width: 125px; height: auto; position: absolute; top: 150px; left: 450px;"
+  />
 </a>
 
-<img 
-  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png" 
+<img
+  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png"
   alt="Flower Image"
-  style="width: 115px; height: auto; position: absolute; top: 550px; left: 350px;" 
+  style="width: 115px; height: auto; position: absolute; top: 550px; left: 350px;"
 />
 
-<img 
-  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png" 
+<img
+  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png"
   alt="Flower Image"
-  style="width: 150px; height: auto; position: absolute; top: 625px; left: 430px; transform: rotate(45deg);" 
+  style="width: 150px; height: auto; position: absolute; top: 625px; left: 430px; transform: rotate(45deg);"
 />
 
-<img 
-  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png" 
+<img
+  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png"
   alt="Flower Image"
-  style="width: 75px; height: auto; position: absolute; top: 650px; left: 1150px;" 
+  style="width: 75px; height: auto; position: absolute; top: 650px; left: 1150px;"
 />
 
-<img 
-  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png" 
+<img
+  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png"
   alt="Flower Image"
-  style="width: 75px; height: auto; position: absolute; top: 650px; left: 1150px;" 
+  style="width: 75px; height: auto; position: absolute; top: 650px; left: 1150px;"
 />
 
-<img 
-  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png" 
+<img
+  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png"
   alt="Flower Image"
-  style="width: 75px; height: auto; position: absolute; top: 275px; left: 1150px;" 
+  style="width: 75px; height: auto; position: absolute; top: 275px; left: 1150px;"
 />
 
-<img 
-  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png" 
+<img
+  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png"
   alt="Flower Image"
-  style="width: 100px; height: auto; position: absolute; top: 175px; left: 1200px;transform: rotate(15deg);" 
+  style="width: 100px; height: auto; position: absolute; top: 175px; left: 1200px; transform: rotate(15deg);"
 />
 
-<img 
-  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png" 
+<img
+  src="https://i.ibb.co/84sq5Ykm/Adobe-Express-file-1.png"
   alt="Flower Image"
-  style="width: 150px; height: auto; position: absolute; top: 250px; left: 1250px;" 
+  style="width: 150px; height: auto; position: absolute; top: 250px; left: 1250px;"
 />
 
-<img 
-  src="https://i.ibb.co/nNKtLjTk/Adobe-Express-file-2.png" 
+<img
+  src="https://i.ibb.co/nNKtLjTk/Adobe-Express-file-2.png"
   alt="Flower Image"
-  style="width: 250px; height: auto; position: absolute; top: 350px; left: 1100px;transform: rotate(150deg)" 
+  style="width: 250px; height: auto; position: absolute; top: 350px; left: 1100px; transform: rotate(150deg)"
 />
 
-<img 
-  src="https://i.ibb.co/nNKtLjTk/Adobe-Express-file-2.png" 
+<img
+  src="https://i.ibb.co/nNKtLjTk/Adobe-Express-file-2.png"
   alt="Flower Image"
-  style="width: 250px; height: auto; position: absolute; top: 125px; left: 975px;transform: rotate(270deg)" 
+  style="width: 250px; height: auto; position: absolute; top: 125px; left: 975px; transform: rotate(270deg)"
 />
 
-<img 
-  src="https://i.ibb.co/nNKtLjTk/Adobe-Express-file-2.png" 
+<img
+  src="https://i.ibb.co/nNKtLjTk/Adobe-Express-file-2.png"
   alt="Flower Image"
-  style="width: 250px; height: auto; position: absolute; top: 400px; left: 300px;transform: rotate(350deg)" 
+  style="width: 250px; height: auto; position: absolute; top: 400px; left: 300px; transform: rotate(350deg)"
 />
 
-<img 
-  src="https://i.ibb.co/nNKtLjTk/Adobe-Express-file-2.png" 
+<img
+  src="https://i.ibb.co/nNKtLjTk/Adobe-Express-file-2.png"
   alt="Flower Image"
-  style="width: 250px; height: auto; position: absolute; top: 600px; left: 550px;transform: rotate(60deg)" 
+  style="width: 250px; height: auto; position: absolute; top: 600px; left: 550px; transform: rotate(60deg)"
 />
 
-<div class="center-wrapper">
-  <form method="POST" class="form-container">
-    <br> <br> <br> <br>
+<!-- ============== CENTERED UI LAYER (strong inline centering) ============== -->
+<div style="position:fixed; inset:0; display:grid; place-items:center; z-index:50;">
+  <div style="display:flex; flex-direction:column; align-items:center; gap:12px; background:rgba(255,255,255,.85); padding:16px 20px; border-radius:12px; box-shadow:0 8px 24px rgba(0,0,0,.12);">
+    <div style="display:flex; gap:12px; align-items:center; justify-content:center; width:100%; flex-wrap:wrap;">
+      <select bind:value={selectedName} required disabled={loading} style="min-width:220px;">
+        <option value="Select User">Select User</option>
+        <option value="Dominic Veneziale">Dominic Veneziale</option>
+        <option value="Julia Meaney">Julia Meaney</option>
+        <option value="Vamu Srinivasan">Vamu Srinivasan</option>
+        <option value="Vedu Srinivasan">Vedu Srinivasan</option>
+        <option value="Varu Srinivasan">Varu Srinivasan</option>
+        <option value="Matthew Aung">Matthew Aung</option>
+        <option value="Gabe Magwood">Gabe Magwood</option>
+        <option value="Kyler Mooney">Kyler Mooney</option>
+        <option value="Maddie Oswald">Maddie Oswald</option>
+        <option value="Cooper Morgan">Cooper Morgan</option>
+        <option value="Joey Kohler">Joey Kohler</option>
+        <option value="Mason Gildein">Mason Gildein</option>
+        <option value="Krupa Dihora">Krupa Dihora</option>
+        <option value="Yusef Seyed">Yusef Seyed</option>
+        <option value="John Coult">John Coult</option>
+        <option value="Mark Hernandez Guerra">Mark Hernandez Guerra</option>
+        <option value="John Coult">John Coult</option>
+        <option value="Dylan Braverman">Dylan Braverman</option>
+        <option value="Jackson Burmeister">Jackson Burmeister</option>
+        <option value="Karsten Immerzeal">Karsten Immerzeal</option>
+        <option value="Stanley Wempe">Stanley Wempe</option>
+        <option value="Victor Bodea">Victor Bodea</option>
+        <option value="Alez Gualfieri">Alez Gualfieri</option>
+        <option value="Ani Hari">Ani Hari</option>
+        <option value="Aubrey Cressnan">Aubrey Cressnan</option>
+        <option value="Ben Snyder">Ben Snyder</option>
+        <option value="Conner Male">Conner Male</option>
+        <option value="Daytona Christman">Daytona Christman</option>
+        <option value="Daniel Kim">Daniel Kim</option>
+        <option value="Emma Husvar">Emma Husvar</option>
+        <option value="Geo Jung">Geo Jung</option>
+        <option value="Grace Russell">Grace Russell</option>
+        <option value="Grant Hufgard">Grant Hufgard</option>
+        <option value="Isabella O.A.">Isabella O.A.</option>
+        <option value="Jake Tsyvine">Jake Tsyvine</option>
+        <option value="Jayee Lin">Jayee Lin</option>
+        <option value="Joey Kholer">Joey Kholer</option>
+        <option value="John Cult">John Cult</option>
+        <option value="Karsten Immerzeel">Karsten Immerzeel</option>
+        <option value="Manas Pol">Manas Pol</option>
+        <option value="Matthew Ryan">Matthew Ryan</option>
+        <option value="Myles Bartholomew">Myles Bartholomew</option>
+        <option value="Paytuna A.">Paytuna A.</option>
+        <option value="Quinn Ham">Quinn Ham</option>
+        <option value="Ritwika Veeravalli">Ritwika Veeravalli</option>
+        <option value="Sebastian Velez">Sebastian Velez</option>
+        <option value="Trace Willaims">Trace Willaims</option>
+        <option value="Vaaman Srinivasan">Vaaman Srinivasan</option>
+        <option value="Vanessa Trbuza">Vanessa Trbuza</option>
+        <option value="Varahan Srinivasan">Varahan Srinivasan</option>
+        <option value="Vedanth Srinivasan">Vedanth Srinivasan</option>
+        <option value="Yusef Syed">Yusef Syed</option>
+        <option value="Akshaya Guduru">Akshaya Guduru</option>
+        <option value="Anand Kunnath">Anand Kunnath</option>
+      </select>
 
-    <select 
-      bind:value={selectedName} 
-      class="select select-bordered w-full max-w-xs" 
-      required
-      disabled={loading}
-    >
-      <option value="Select User">Select User</option>
-      <option value="Dominic Veneziale">Dominic Veneziale</option>
-      <option value="Julia Meaney">Julia Meaney</option>
-      <option value="Vamu Srinivasan">Vamu Srinivasan</option>
-      <option value="Vedu Srinivasan">Vedu Srinivasan</option>
-      <option value="Varu Srinivasan">Varu Srinivasan</option>
-      <option value="Matthew Aung">Matthew Aung</option>
-      <option value="Gabe Magwood">Gabe Magwood</option>
-      <option value="Kyler Mooney">Kyler Mooney</option>
-      <option value="Maddie Oswald">Maddie Oswald</option>
-      <option value="Cooper Morgan">Cooper Morgan</option>
-      <option value="Joey Kohler">Joey Kohler</option>
-      <option value="Mason Gildein">Mason Gildein</option>
-      <option value="Krupa Dihora">Krupa Dihora</option>
-      <option value="Yusef Seyed">Yusef Seyed</option>
-      <option value="John Coult">John Coult</option>
-      <option value="Mark Hernandez Guerra">Mark Hernandez Guerra</option>
-      <option value="John Coult">John Coult</option>
-      <option value="Dylan Braverman">Dylan Braverman</option>
-      <option value="Jackson Burmeister">Jackson Burmeister</option>
-      <option value="Karsten Immerzeal">Karsten Immerzeal</option>
-      <option value="Stanley Wempe">Stanley Wempe</option>
-      <option value="Victor Bodea">Victor Bodea</option>
-      <option value="Alez Gualfieri">Alez Gualfieri</option>
-      <option value="Ani Hari">Ani Hari</option>
-      <option value="Aubrey Cressnan">Aubrey Cressnan</option>
-      <option value="Ben Snyder">Ben Snyder</option>
-      <option value="Conner Male">Conner Male</option>
-      <option value="Daytona Christman">Daytona Christman</option>
-      <option value="Daniel Kim">Daniel Kim</option>
-      <option value="Emma Husvar">Emma Husvar</option>
-      <option value="Geo Jung">Geo Jung</option>
-      <option value="Grace Russell">Grace Russell</option>
-      <option value="Grant Hufgard">Grant Hufgard</option>
-      <option value="Isabella O.A.">Isabella O.A.</option>
-      <option value="Jake Tsyvine">Jake Tsyvine</option>
-      <option value="Jayee Lin">Jayee Lin</option>
-      <option value="Joey Kholer">Joey Kholer</option>
-      <option value="John Cult">John Cult</option>
-      <option value="Karsten Immerzeel">Karsten Immerzeel</option>
-      <option value="Manas Pol">Manas Pol</option>
-      <option value="Matthew Ryan">Matthew Ryan</option>
-      <option value="Myles Bartholomew">Myles Bartholomew</option>
-      <option value="Paytuna A.">Paytuna A.</option>
-      <option value="Quinn Ham">Quinn Ham</option>
-      <option value="Ritwika Veeravalli">Ritwika Veeravalli</option>
-      <option value="Sebastian Velez">Sebastian Velez</option>
-      <option value="Trace Willaims">Trace Willaims</option>
-      <option value="Vaaman Srinivasan">Vaaman Srinivasan</option>
-      <option value="Vanessa Trbuza">Vanessa Trbuza</option>
-      <option value="Varahan Srinivasan">Varahan Srinivasan</option>
-      <option value="Vedanth Srinivasan">Vedanth Srinivasan</option>
-      <option value="Yusef Syed">Yusef Syed</option>
-      <option value="Akshaya Guduru">Akshaya Guduru</option>
-      <option value="Anand Kunnath">Anand Kunnath</option>
-    </select>
-    <br>
+      <button
+        type="button"
+        class="btn py-2 px-4 text-sm"
+        on:click={() => submitAttendance("Sign In")}
+        disabled={loading}
+      >
+        {loading ? "Processing..." : "Sign In"}
+      </button>
 
-    <button 
-      type="button" 
-      class="btn py-2 px-4 text-sm" 
-      on:click={() => submitAttendance("Sign In")}
-      disabled={loading}
-    >
-      {loading ? "Processing..." : "Sign In"}
-    </button>
-
-    <button 
-      type="button" 
-      class="btn py-2 px-4 text-sm" 
-      on:click={() => submitAttendance("Sign Out")}
-      disabled={loading}
-    >
-      {loading ? "Processing..." : "Sign Out"}
-    </button>
-
-    <br> <br> <br>
+      <button
+        type="button"
+        class="btn py-2 px-4 text-sm"
+        on:click={() => submitAttendance("Sign Out")}
+        disabled={loading}
+      >
+        {loading ? "Processing..." : "Sign Out"}
+      </button>
+    </div>
 
     <canvas
       bind:this={canvas}
@@ -305,8 +278,20 @@
       on:touchmove={draw}
       on:touchend={stopDrawing}
       on:touchcancel={stopDrawing}
+      style="border:8px solid #ffffff; background:#4169e1; cursor:crosshair; margin-top:6px;"
     ></canvas>
-    <br>
+
     <button type="button" class="btn" on:click={clearCanvas}>Clear</button>
-  </form>
+  </div>
 </div>
+
+<style>
+  /* keep only your canvas defaults (images are inline-styled already) */
+  canvas {
+    /* these are overridden by inline styles but left for clarity */
+    border: 8px solid #ffffff;
+    background-color: royalblue;
+    cursor: crosshair;
+    margin-top: 20px;
+  }
+</style>
